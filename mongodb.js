d8f24683eb3c0392db7d9c6a952c60a3183a8631
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-09-29 16:46:38 +0200

    Setup connection with mongoDB database. Insert new data to database

---
+++ mongodb.js
@@ -0,0 +1,32 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('tasks').insertMany(
+      [
+        { description: 'Learn Node.js', completed: false },
+        { description: 'Learng JavaScript', completed: true },
+        { description: 'Learn better Redux', completed: false },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert new data to database');
+        }
+
+        console.log(result.ops);
+      }
+    );
+  }
+);
