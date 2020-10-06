572416f6dfd57359e9dc85212f2729bd34e802fe
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-10-06 16:04:23 +0200

    Data pagination and sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,9 +19,15 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
   }
 
   try {
@@ -30,6 +36,11 @@ router.get('/tasks', auth, async (req, res) => {
       .populate({
         path: 'tasks',
         match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
       })
       .execPopulate();
     res.send(req.user.tasks);
