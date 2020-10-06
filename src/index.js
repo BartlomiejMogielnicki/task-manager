875c16cb1bf2a02a75718a742cbe30f5d3c6635d
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-10-06 14:44:08 +0200

    Handle tasks endpoints for logged in user only. Delete all user tasks when user is removed

--- src/index.js
+++ src/index.js
@@ -6,19 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware functon
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('GET requests are disabled');
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send('Server is in maintenance. Sorry.');
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
