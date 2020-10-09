fc7babcd882205282dd69103767b2dc7d46636e9
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-10-09 11:04:08 +0200

    Add example tests to check response body and database data

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
+const { response } = require('express');
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -23,7 +24,7 @@ beforeEach(async () => {
 });
 
 test('Should signup a new user', async () => {
-  await request(app)
+  const response = await request(app)
     .post('/users')
     .send({
       name: 'Monica',
@@ -31,16 +32,32 @@ test('Should signup a new user', async () => {
       password: 'shouldbeok',
     })
     .expect(201);
+
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  expect(response.body).toMatchObject({
+    user: {
+      name: 'Monica',
+      email: 'monica@example.com',
+    },
+    token: user.tokens[0].token,
+  });
+
+  expect(user.password).not.toBe('shouldbeok');
 });
 
 test('Should login existing user', async () => {
-  await request(app)
+  const response = await request(app)
     .post('/users/login')
     .send({
       email: userOne.email,
       password: userOne.password,
     })
     .expect(200);
+
+  const user = await User.findById(response.body.user._id);
+  expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test('Should not login nonexistent user', async () => {
@@ -66,11 +83,14 @@ test('Should not get profile for unauthenticated user', async () => {
 });
 
 test('Should delete account for user', async () => {
-  await request(app)
+  const response = await request(app)
     .delete('/users/me')
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user).toBeNull();
 });
 
 test('Should not delete account for unauthenticated user', async () => {
